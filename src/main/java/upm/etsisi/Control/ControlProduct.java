8b94c0d4603dc7501bdf238b10cb16f6c4beae99
tinay <zhuoyue.ye@alumnos.upm.es>
2025-10-01 10:41:47 +0200

    tina

--- src/main/java/upm/etsisi/Control/ControlProduct.java
+++ src/main/java/upm/etsisi/Control/ControlProduct.java
@@ -20,7 +20,7 @@ public class ControlProduct {
 
     public boolean existProduct(String id, String name, double price, String category) {
         boolean exist = false;
-        for (int i = 0; i < productList.size(); i++) {
+        for(int i = 0; i < productList.size(); i++) {
             if (productList.get(i) != null) {
                 if (productList.get(i).getId().equalsIgnoreCase(id) && productList.get(i).getName().equalsIgnoreCase(name) &&
                         productList.get(i).getPrice()==price && productList.get(i).getCategory().equals(category) ){
@@ -34,18 +34,21 @@ public class ControlProduct {
 
     public boolean addId(Product product) {
         boolean result = false;
-        if(!existProduct(product.getId(),product.getName(),product.getPrice(),product.getCategory())){
-            productList.add(product);
-            numProducts++;
-            result = true;
+        if(productList.size() < MAX_PRODUCT){
+            if(!existProduct(product.getId(),product.getName(),product.getPrice(),product.getCategory())){
+                productList.add(product);
+                numProducts++;
+                result = true;
+            }
         }
+
         return result;
     }
 
     public boolean removeProduct(String id) {
         boolean resul = false;
         for (int i = 0; i < productList.size(); i++) {
-            if (productList.get(i).equals(id)) {
+            if (productList.get(i).getId().equals(id)) {
                 productList.remove(productList.get(i));
                 numProducts--;
                 resul = true;
