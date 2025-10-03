050fc3a5d51f833016114a5f8d3d7598a0c2627a
pablolmg <pablo.lopez285@educa.madrid.org>
2025-10-03 09:04:50 +0200

    Ticket

--- src/main/java/upm/etsisi/Control/ControlTicket.java
+++ src/main/java/upm/etsisi/Control/ControlTicket.java
@@ -1,11 +1,88 @@
 package upm.etsisi.Control;
 
 import upm.etsisi.Model.Product;
+import upm.etsisi.Model.Ticket;
 
-import java.util.List;
+import java.util.Map;
 
 public class ControlTicket {
-    private List<Product> products;
+    //private List<Product> products;
+    //private final int MAX_PRODUCT = 100;
 
-    private final int MAX_PRODUCT = 100;
+    private Ticket ticket;
+
+    public ControlTicket(Ticket ticket) {
+        this.ticket = new Ticket();
+    }
+
+    public void newTicket() {
+        this.ticket = new Ticket();
+        System.out.println("ticket new: ok");
+    }
+
+    public void addProduct(Product product, int amount) {
+        Map<Product, Integer> items = ticket.getTicket();
+        items.put(product, amount);
+        calculateTotal();
+        printTicket();
+        System.out.println("ticket added: ok");
+    }
+
+    public void removeProduct(Product product) {
+        Map<Product, Integer> items = ticket.getTicket();
+        items.remove(product);
+        calculateTotal();
+        printTicket();
+        System.out.println("ticket removed: ok");
+    }
+
+    private void printTicket() {
+        Map<Product, Integer> items = ticket.getTicket();
+
+        for (Map.Entry<Product, Integer> entry : items.entrySet()) {
+            Product product = entry.getKey();
+            //////   product.toString();
+        }
+
+        System.out.println("Total price: " + ticket.getTotal());
+        System.out.println("Total discount: " + ticket.getDiscount());
+        System.out.println("Final Price: " + ticket.getFinalPrice());
+        System.out.println("ticket print: ok");
+
+    }
+
+    private void calculateTotal() {
+        Map<Product, Integer> items = ticket.getTicket();
+        double total = 0.0;
+        double discount = 0.0;
+
+        for (Map.Entry<Product, Integer> entry : items.entrySet()) {
+            Product product=  entry.getKey();
+            int amount = entry.getValue();
+            total += amount * product.getPrice();
+
+            String category = product.getCategory();
+            switch (category) {
+                case "MERCH":
+                    discount += amount * 1 * product.getPrice();
+                    break;
+                case "PAPELERIA":
+                    discount += amount * 0.05 * product.getPrice();
+                    break;
+                case "ROPA":
+                    discount += amount * 0.07 * product.getPrice();
+                    break;
+                case "LIBRO":
+                    discount += amount * 0.10 * product.getPrice();
+                    break;
+                case "ELECTRONICA":
+                    discount += amount * 0.03 * product.getPrice();
+                    break;
+            }
+        }
+        ticket.setDiscount(discount);
+        ticket.setTotal(total);
+
+        ticket.setFinalPrice(total-discount);
+    }
 }
--- src/main/java/upm/etsisi/Model/Ticket.java
+++ src/main/java/upm/etsisi/Model/Ticket.java
@@ -1,4 +1,50 @@
 package upm.etsisi.Model;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Ticket {
+    private Map<Product, Integer> ticket;
+    private double total;
+    private double discount;
+    private double finalPrice;
+
+    public Ticket() {
+        this.ticket = new HashMap<Product, Integer>();
+        this.total = 0;
+        this.discount = 0;
+        this.finalPrice = 0;
+    }
+
+    public void setTicket(Map<Product, Integer> ticket) {
+        this.ticket = ticket;
+    }
+
+    public Map<Product, Integer> getTicket() {
+        return ticket;
+    }
+
+    public void setTotal(double total) {
+        this.total = total;
+    }
+
+    public double getTotal() {
+        return total;
+    }
+
+    public void setDiscount(double discount) {
+        this.discount = discount;
+    }
+
+    public double getDiscount() {
+        return discount;
+    }
+
+    public void setFinalPrice(double finalPrice) {
+        this.finalPrice = finalPrice;
+    }
+
+    public double getFinalPrice() {
+        return finalPrice;
+    }
 }
