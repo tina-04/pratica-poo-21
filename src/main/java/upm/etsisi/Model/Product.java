977a4a9618f736919b462f7c8ce206bc838ead55
tinay <zhuoyue.ye@alumnos.upm.es>
2025-10-08 10:17:27 +0200

    tina

--- src/main/java/upm/etsisi/Model/Product.java
+++ src/main/java/upm/etsisi/Model/Product.java
@@ -4,8 +4,6 @@ import upm.etsisi.Utility.Category;
 
 
 public class Product {
-
-
     private int id;
     private Category category;
     private String name;
@@ -22,37 +20,27 @@ public class Product {
         }
     }
 
-
-
     public Category getCategory() {
         return category;
     }
-
     public void setCategory(Category category) {
         this.category = category;
     }
-
     public String getName() {
         return name;
     }
-
     public void setName(String name) {
         this.name = name;
     }
-
     public double getPrice() {
         return price;
     }
-
     public void setPrice(double price) {
         this.price = price;
     }
-
-
     public int getId() {
         return id;
     }
-
     public void setId(int id) {
         this.id = id;
     }
