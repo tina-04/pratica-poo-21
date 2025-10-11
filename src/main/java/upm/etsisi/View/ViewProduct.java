d28ac2fe7d26fbb459bb477a342a0da66a7f66d2
tinay <zhuoyue.ye@alumnos.upm.es>
2025-10-11 15:57:24 +0200

    tina

--- src/main/java/upm/etsisi/View/ViewProduct.java
+++ src/main/java/upm/etsisi/View/ViewProduct.java
@@ -8,20 +8,18 @@ public class ViewProduct {
     public void messageOutput(String output) {
         System.out.println(output);
     }
-    public void printProduct(Product product) {
 
-        if(product != null){
-            messageOutput("{class:Product: id: " + product.getId()+ ", name : '" + product.getName()+
-                    "', category : " + product.getCategory()+ ", price : " + product.getPrice() + "}");
-        }
+    public void printProduct(Product product) {
+        messageOutput("{class:Product: id: " + product.getId() + ", name : '" + product.getName() +
+                "', category : " + product.getCategory() + ", price : " + product.getPrice() + "}");
+
 
     }
 
-    public void listProduct(List<Product> productList){
-        for(Product product : productList){
-            if(product != null){
-                messageOutput("{class:Product: id: " + product.getId()+ "name : '" + product.getName()+ "', category : " +
-                        product.getCategory()+ ", price : " + product.getPrice() + " }");
+    public void listProduct(List<Product> productList) {
+        for (Product product : productList) {
+            if (product != null) {
+                printProduct(product);
             }
         }
 
