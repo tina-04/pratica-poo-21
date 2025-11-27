594e1795795ae7498787a3845c4cf3e50e852e6d
NachoM11 <ignacio.morenoviruete@alumnos.upm.es>
2025-11-27 12:39:40 +0100

    .

--- src/main/java/upm/etsisi/Commands/client/ClientCommandRemove.java
+++ src/main/java/upm/etsisi/Commands/client/ClientCommandRemove.java
@@ -3,6 +3,7 @@ package upm.etsisi.Commands.client;
 import upm.etsisi.Commands.Command;
 import upm.etsisi.Control.ControlClient;
 import upm.etsisi.Control.ControlProduct;
+import upm.etsisi.Control.ControlTicket;
 
 public class ClientCommandRemove extends Command {
     public ClientCommandRemove() {
@@ -12,7 +13,7 @@ public class ClientCommandRemove extends Command {
     public boolean apply(String[] args) {
         boolean result = false;
         if (args[1].equals("remove")) {
-            ControlProduct.getInstance().removeProduct(Integer.parseInt(args[4]));
+            ControlClient.getInstance().removeClient((args[4]));
             result= true;
         }
         return result;
