9624683fc09e9fc3b89eecf19e8de42047db36b6
pablolmg <pablo.lopez285@educa.madrid.org>
2025-11-30 19:46:09 +0100

    User

--- src/main/java/upm/etsisi/Model/Cashier.java
+++ src/main/java/upm/etsisi/Model/Cashier.java
@@ -1,55 +1,18 @@
 package upm.etsisi.Model;
 
-import java.util.ArrayList;
 import java.util.List;
 
-public class Cashier  {
-
-    private String name;
-    private String email;
-
-    private String cashierId;
-
-
-    private List<Ticket> tikcetList;
+public class Cashier extends User {
 
     public Cashier(String cashierId, String name, String email) {
-        this.cashierId = cashierId;
-        this.name = name;
-        this.email = email;
-        this.tikcetList= new ArrayList<>();
-    }
-    public List<Ticket> getTikcetList() {
-        return tikcetList;
-    }
-
-    public void setTikcetList(List<Ticket> tikcetList) {
-        this.tikcetList = tikcetList;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
+        super(cashierId, name, email);
     }
 
     public String getCashierId() {
-        return cashierId;
+        return this.id;
     }
 
     public void setCashierId(String cashierId) {
-        this.cashierId = cashierId;
+        this.id = cashierId;
     }
-
-
 }
--- src/main/java/upm/etsisi/Model/Client.java
+++ src/main/java/upm/etsisi/Model/Client.java
@@ -1,44 +1,13 @@
 package upm.etsisi.Model;
 
-
-import java.util.ArrayList;
-import java.util.List;
-
-public class Client  {
+public class Client extends User {
     private String DNI;
-    private String email;
-    private String name;
     private String cashierId;
-    private List<Ticket> ticketList;
 
     public Client(String name, String DNI, String email, String cashierId) {
-        this.name = name;
+        super(DNI, name, email);
         this.DNI = DNI;
-        this.email = email;
         this.cashierId = cashierId;
-        this.ticketList=new ArrayList<>();
-    }
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-    public List<Ticket> getTicketList() {
-        return ticketList;
-    }
-
-    public void setTicketList(List<Ticket> ticketList) {
-        this.ticketList = ticketList;
     }
     public String getDNI() {
         return DNI;
@@ -46,12 +15,14 @@ public class Client  {
 
     public void setDNI(String DNI) {
         this.DNI = DNI;
+        this.id = DNI;
     }
 
     public String getCashierId() {
         return cashierId;
     }
-    public void setCashierId(String id) {
-        this.cashierId = id;
+
+    public void setCashierId(String cashierId) {
+        this.cashierId = cashierId;
     }
 }
