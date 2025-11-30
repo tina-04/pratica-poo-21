a715af67f3c5404de6c155a424bdb51fa5aa3aa0
Usuario <alejandro.cnavea@alumnos.upm.es>
2025-11-30 02:05:46 +0100

    Cambio ViewApp

--- src/main/java/upm/etsisi/View/ViewApp.java
+++ src/main/java/upm/etsisi/View/ViewApp.java
@@ -18,20 +18,29 @@ public class ViewApp  implements View {
 
     public static void printHelp() {
         System.out.println("Commands:");
+        System.out.println("  client add \"<name>\" <DNI> <email> <cashId>");
+        System.out.println("  client remove <DNI>");
+        System.out.println("  client list");
+        System.out.println("  cash add [<id>] \"<name>\" <email>");
+        System.out.println("  cash remove <id>");
+        System.out.println("  cash list");
+        System.out.println("  cash tickets <id>");
+        System.out.println("  ticket new [<id>] <cashId> <userId>");
+        System.out.println("  ticket add <ticketId> <cashId> <prodId> <amount> [--p<txt> --p<txt>]");
+        System.out.println("  ticket remove <ticketId> <cashId> <prodId>");
+        System.out.println("  ticket print <ticketId> <cashId>");
+        System.out.println("  ticket list");
         System.out.println("  prod add <id> \"<name>\" <category> <price>");
-        System.out.println("  prod list");
         System.out.println("  prod update <id> NAME|CATEGORY|PRICE <value>");
+        System.out.println("  prod addFood [<id>] \"<name>\" <price> <expiration:yyyy-MM-dd> <max_people>");
+        System.out.println("  prod addMeeting [<id>] \"<name>\" <price> <expiration:yyyy-MM-dd> <max_people>");
+        System.out.println("  prod list");
         System.out.println("  prod remove <id>");
-        System.out.println("  ticket new");
-        System.out.println("  ticket add <prodId> <quantity>");
-        System.out.println("  ticket remove <prodId>");
-        System.out.println("  ticket print");
-        System.out.println("  echo \"<texto>\"");
         System.out.println("  help");
+        System.out.println("  echo “<text>”");
         System.out.println("  exit");
-        System.out.println("\nCategories: MERCH, STATIONERY, CLOTHES, BOOK, ELECTRONICS");
-        System.out.println("Discounts if there are ≥2 units in the category: MERCH 0%, PAPELERIA 5%, ROPA 7%, LIBRO 10%, ELECTRONICA 3%.");
     }
+
 
     public static void init()
     {
