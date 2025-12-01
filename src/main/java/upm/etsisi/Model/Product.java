0b10cfd349436871cd26d9b85eb8f29ecdcc0f8b
Usuario <alejandro.cnavea@alumnos.upm.es>
2025-12-01 17:46:29 +0100

    .

--- src/main/java/upm/etsisi/Model/Product.java
+++ src/main/java/upm/etsisi/Model/Product.java
@@ -1,120 +1,49 @@
 package upm.etsisi.Model;
 
-import upm.etsisi.Utility.Category;
-import upm.etsisi.Utility.ProductType;
+public abstract class Product {
+    protected Integer id;
+    protected String name;
+    protected double price;
 
-import java.time.LocalDate;
-
-
-public class Product {
-    private Integer id;
-    private Category category;
-    private String name;
-    private double price;
-
-    private LocalDate expiration;
-    private int maxPersonal;
-    private String personalizationList;
-    private ProductType productType;
-
-
-
-    private int actualPeople;
-
-
-    public Product(Integer id, String name, Category category, double price) {
+    public Product(Integer id, String name, double price) {
         this.id = id;
-        if(name.length() < 100 && !name.isEmpty()){
+        if (name != null && name.length() < 100 && !name.isEmpty()) {
             this.name = name;
-        }else{
+        } else {
             this.name = null;
         }
-        this.category = category;
-        if(price > 0){
+        if (price > 0) {
             this.price = price;
         }
     }
 
-    public Product(Integer id, String name,  Category category , double price, LocalDate expiration ,Integer maxPersonal, String personalizationList ) {
-        this.id = id;
-        if(name.length() < 100 && !name.isEmpty()){
-            this.name = name;
-        }else{
-            this.name = null;
-        }
-        if(price > 0){
-            this.price = price;
-        }
-        this.category =category;
-        this.maxPersonal = maxPersonal;
-        this.personalizationList=personalizationList;
-        this.expiration = expiration;
-        this.productType=null;
-        this.actualPeople = 0;
-    }
-    public void setMaxPersonal(int maxPersonal) {
-        this.maxPersonal = maxPersonal;
-    }
+    public abstract double calculatePrice(int quantity);
 
-    public int getActualPeople() {
-        return actualPeople;
-    }
-
-    public void setActualPeople(int acutalPeople) {
-        this.actualPeople = acutalPeople;
-    }
-    public String getPersonalizationList() {
-        return personalizationList;
-    }
-
-    public void setPersonalizationList(String list) {
-        this.personalizationList = list;
-    }
-    public LocalDate getExpiration() {
-        return expiration;
-    }
-
-    public void setExpiration(LocalDate date) {
-        this.expiration = date;
-    }
-    public Category getCategory() {
-        return category;
-    }
-    public void setCategory(Category category) {
-        this.category = category;
-    }
-    public String getName() {
-        return name;
-    }
-    public void setName(String name) {
-        this.name = name;
-    }
-    public double getPrice() {
-        return price;
-    }
-    public void setPrice(double price) {
-        this.price = price;
-    }
     public Integer getId() {
         return id;
     }
+
     public void setId(Integer id) {
         this.id = id;
     }
-    public Integer getMaxPersonal() {
-        return maxPersonal;
+
+    public String getName() {
+        return name;
     }
 
-    public void setMaxPersonal(Integer maxPersonal) {
-        this.maxPersonal = maxPersonal;
+    public void setName(String name) {
+        if (name != null && name.length() < 100 && !name.isEmpty()) {
+            this.name = name;
+        }
     }
 
-    public ProductType getProductType() {
-        return productType;
+    public double getPrice() {
+        return price;
     }
 
-    public void setProductType(ProductType type) {
-        this.productType = type;
+    public void setPrice(double price) {
+        if (price > 0) {
+            this.price = price;
+        }
     }
-
 }
