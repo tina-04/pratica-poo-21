4e86b579fc5eeb3663b9553723165fdc48df87e0
Usuario <alejandro.cnavea@alumnos.upm.es>
2025-12-01 17:45:09 +0100

    .

---
+++ src/main/java/upm/etsisi/Model/TimedProduct.java
@@ -0,0 +1,56 @@
+package upm.etsisi.Model;
+
+import upm.etsisi.Utility.ProductType;
+import java.time.LocalDate;
+
+public class TimedProduct extends Product {
+    private LocalDate expiration;
+    private int maxPersonal;
+    private ProductType productType;
+    private int acutalPeople;
+
+    public TimedProduct(Integer id, String name, double price, LocalDate expiration, int maxPersonal, ProductType productType) {
+        super(id, name, price);
+        this.expiration = expiration;
+        this.maxPersonal = maxPersonal;
+        this.productType = productType;
+        this.acutalPeople = 0;
+    }
+
+    @Override
+    public double calculatePrice(int quantity) {
+        return price * quantity;
+    }
+
+    public LocalDate getExpiration() {
+        return expiration;
+    }
+
+    public void setExpiration(LocalDate expiration) {
+        this.expiration = expiration;
+    }
+
+    public int getMaxPersonal() {
+        return maxPersonal;
+    }
+
+    public void setMaxPersonal(int maxPersonal) {
+        this.maxPersonal = maxPersonal;
+    }
+
+    public ProductType getProductType() {
+        return productType;
+    }
+
+    public void setProductType(ProductType productType) {
+        this.productType = productType;
+    }
+
+    public int getAcutalPeople() {
+        return acutalPeople;
+    }
+
+    public void setAcutalPeople(int acutalPeople) {
+        this.acutalPeople = acutalPeople;
+    }
+}
