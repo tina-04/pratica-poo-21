ea4a457139d53b2096dbd79dd80d9cadb56d5d64
Usuario <alejandro.cnavea@alumnos.upm.es>
2025-12-01 15:00:10 +0100

    .

--- src/main/java/upm/etsisi/View/ViewTicket.java
+++ src/main/java/upm/etsisi/View/ViewTicket.java
@@ -1,8 +1,10 @@
 package upm.etsisi.View;
 
 import upm.etsisi.Control.ControlTicket;
+import upm.etsisi.Model.BasicProduct;
 import upm.etsisi.Model.Product;
 import upm.etsisi.Model.Ticket;
+import upm.etsisi.Model.TimedProduct;
 import upm.etsisi.Utility.Category;
 import upm.etsisi.Utility.ProductType;
 
@@ -18,46 +20,61 @@ public class ViewTicket  implements View{
         messageOutput("Ticket:" + ticket.getId());
     }
     public void printProductBasic(Product product) {
-        if(product != null){
-            messageOutput("{class:Product, id:" + product.getId()+ ", name:'" + product.getName()+
-                    "', category:" + product.getCategory()+ ", price:" + product.getPrice() + "}");
+        if(product instanceof BasicProduct){
+            BasicProduct basicProd = (BasicProduct) product;
+
+            messageOutput("{class:Product, id:" + basicProd.getId()+ ", name:'" + basicProd.getName()+
+                    "', category:" + basicProd.getCategory()+ ", price:" + basicProd.getPrice() + "}");
         }
 
     }
     public void printProductDiscount(Product product, double discount) { // Locale.US imprime . en vez de , para los decimales
+        BasicProduct basicProd = (BasicProduct) product;
 
-        messageOutput("{class:Product, id:" + product.getId() + ", name:'" + product.getName() + "', category:" +
-                product.getCategory() + ", price:" + String.format(Locale.US, "%.2f", product.getPrice()) + "} **discount -" + String.format(Locale.US, "%.2f", discount));
+        messageOutput("{class:Product, id:" + basicProd.getId() + ", name:'" + basicProd.getName() + "', category:" +
+                basicProd.getCategory() + ", price:" + String.format(Locale.US, "%.2f", basicProd.getPrice()) + "} **discount -" + String.format(Locale.US, "%.2f", discount));
     }
     public void printProductDiscountPersonlization(Product product, double discount) {
+        BasicProduct basicProd = (BasicProduct) product;
 
-        messageOutput("{class:ProductPersonalized, id:" + product.getId() + ", name:'" + product.getName() + "', category:" +
-                product.getCategory() + ", price:" + String.format(Locale.US,"%.2f", product.getPrice()) +  "， personalizationList:[" + product.getPersonalizationList()+"]} **discount -" + String.format(Locale.US, "%.2f", discount));
+        messageOutput("{class:ProductPersonalized, id:" + basicProd.getId() + ", name:'" + basicProd.getName() + "', category:" +
+                basicProd.getCategory() + ", price:" + String.format(Locale.US,"%.2f", basicProd.getPrice()) +  "， personalizationList:[" + basicProd.getPersonalizationList()+"]} **discount -" + String.format(Locale.US, "%.2f", discount));
     }
     public void printProductPersonalization(Product product) {
-        if(product.getPersonalizationList() ==null){
-            messageOutput("{class:ProductPersonalized, id:" + product.getId() + ", name:'" + product.getName() + "', category:" +
-                    product.getCategory() + ", price:" + String.format(Locale.US,"%.2f", product.getPrice()) +"}");
-        }else{
-            messageOutput("{class:ProductPersonalized, id:" + product.getId() + ", name:'" + product.getName() + "', category:" +
-                    product.getCategory() + ", price:" + String.format(Locale.US,"%.2f", product.getPrice()) +  "， personalizationList：[" + product.getPersonalizationList()+"]}");
+        if (product instanceof BasicProduct) {
+            BasicProduct basicProduct = (BasicProduct) product;
+
+            if (basicProduct.getPersonalizationList() == null) {
+                messageOutput("{class:ProductPersonalized, id:" + basicProduct.getId() +
+                        ", name:'" + basicProduct.getName() +
+                        "', category:" + basicProduct.getCategory() +
+                        ", price:" + String.format(Locale.US, "%.2f", basicProduct.getPrice()) +
+                        "}");
+            } else {
+                messageOutput("{class:ProductPersonalized, id:" + basicProduct.getId() +
+                        ", name:'" + basicProduct.getName() +
+                        "', category:" + basicProduct.getCategory() +
+                        ", price:" + String.format(Locale.US, "%.2f", basicProduct.getPrice()) +
+                        ", personalizationList:[" + basicProduct.getPersonalizationList() + "]" +
+                        "}");
+            }
         }
-
-
     }
     public void printProductFood(Product product, int actualPeople) {
-        if(product != null){
-            messageOutput("{class:Food, id:" + product.getId()+ ", name: '" + product.getName()+
-                    ", price:" + product.getPrice() +  ", date of Event:"+product.getExpiration()+
-                    ",max people allowed:"+product.getMaxPersonal()+",actual people in event;"+ actualPeople+"}");
+        if(product instanceof TimedProduct){
+            TimedProduct timedProduct = (TimedProduct) product;
+            messageOutput("{class:Food, id:" + timedProduct.getId()+ ", name: '" + timedProduct.getName()+
+                    ", price:" + timedProduct.getPrice() +  ", date of Event:"+timedProduct.getExpiration()+
+                    ",max people allowed:"+timedProduct.getMaxPersonal()+",actual people in event;"+ actualPeople+"}");
         }
 
     }
     public void printProductMeeting(Product product, int actualPeople) {
-        if(product != null){
-            messageOutput("{class:Meeting, id:" + product.getId()+ ", name: '" + product.getName()+
-                    "', price:" + product.getPrice() +  ", date of Event:"+product.getExpiration()+
-                    ",max people allowed:"+product.getMaxPersonal()+ ", actual people in event:" + actualPeople+"}");
+        if(product instanceof TimedProduct){
+            TimedProduct timedProduct = (TimedProduct) product;
+            messageOutput("{class:Meeting, id:" + timedProduct.getId()+ ", name: '" + timedProduct.getName()+
+                    "', price:" + timedProduct.getPrice() +  ", date of Event:"+timedProduct.getExpiration()+
+                    ",max people allowed:"+timedProduct.getMaxPersonal()+ ", actual people in event:" + actualPeople+"}");
         }
     }
 
